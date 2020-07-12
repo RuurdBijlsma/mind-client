87d1b84c50bcab619f175a245452f6a44bedb7e8
mrmusuadhi <m.r.musuadhi.rajan@student.rug.nl>
2020-07-12 05:24:27 +0200

    mindcardvue update

--- src/components/MindCard.vue
+++ src/components/MindCard.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="card hidden" v-if="hide"
-         :style="`opacity: ${this.discarded ? 0 : 1}`">
+    <div class="card hidden" v-if="hide">
     </div>
+    <div class="card star" v-else-if="star" :clickable="$listeners.hasOwnProperty('click')" @click="$emit('click')">
+    </div>
+    <div class="card life" v-else-if="life">
+    </div>
+
     <div class="card" v-else :clickable="$listeners.hasOwnProperty('click')" @click="$emit('click')">
         <div class="top-number">
             <span>{{value}}</span>
@@ -29,7 +33,11 @@
                 type: Boolean,
                 default: false,
             },
-            discarded: {
+            star: {
+                type: String,
+                default: false,
+            },
+            life: {
                 type: Boolean,
                 default: false,
             },
@@ -39,6 +47,8 @@
         },
     }
 </script>
+
+
 <style scoped>
     .card {
         background-color: white;
@@ -50,12 +60,12 @@
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-        height: 210px;
-        width: 150px;
-        min-width: 100px;
-        box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.3);
-        border-radius: 15px;
-        transition: transform 0.2s, opacity 0.5s, width 0.5s, min-width 0.5s;
+        height: 15vh;
+        width: 5vw;
+        min-width: 10vh;
+        box-shadow: 0 0.4rem 1rem 0 rgba(0, 0, 0, 0.3);
+        border-radius: 1rem;
+        transition: transform 0.2s;
         text-align: center;
     }
 
@@ -68,24 +78,39 @@
     }
 
     .card[clickable='true']:hover {
-        transform: translateY(-15px);
+        transform: translateY(-1.0em);
     }
 
     .hidden {
         background-image: url("../assets/card-back.png");
     }
 
-    .big-number {
-        font-size: 60px;
+    .star {
+        background-image: url("../assets/card-star.png");
+    }
+
+    .life {
+        background-image: url("../assets/card-life.png");
+    }
+
+    .displaystar {
+        /* font-size: 60px;
         font-weight: bold;
-        text-shadow: 0 0 20px white;
+        text-shadow: 0 0 2em white;
+        left:50%; */
+    }
+
+    .big-number {
+        font-size: 1rem;
+        font-weight: bold;
+        text-shadow: 0 0 0.5rem white;
     }
 
     .bottom-number, .top-number {
-        font-size: 20px;
+        font-size: 0.5rem;
         display: flex;
         justify-content: space-between;
-        padding: 10px;
+        padding: 0.5rem;
         color: white;
     }
 
